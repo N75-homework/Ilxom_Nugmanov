ec969554d139c94001df8c7ee9447252b06d5aca
IlxomjonNugmanov <nugmanovilxom2003@gmail.com>
2022-11-07 17:52:52 +0500

    app

---
+++ app.js
@@ -0,0 +1,91 @@
+// ~~~1~~~
+// Comment
+
+/*Comment 
+qatorlari*/
+
+// ~~~2~~~
+
+var lastName = 10;
+var ourName = 'Ilxom';
+var numString = '10';
+var a = null; 
+var b = undefined;
+var c = true;
+var d = fox = {
+
+} ;
+var e = 10n;
+var f = app = [
+
+];
+
+// ~~~3~~~
+
+// var myVar ;
+// myVar = 7;
+// console.log(myVar);
+
+
+// ~~~4~~~
+
+// var a;
+// a = 5;
+// var b;
+// b = a;
+// console.log(b);
+
+
+// ~~~5~~~
+
+// var a = 9;
+// console.log(a);
+
+// ~~~6~~~
+
+// var myFirstName = 'ilxpom';
+// var myLastName = 'lekjdbl';
+
+// ~~~7~~~
+
+// var a;
+// var b;
+// var c;
+
+// a = a + 6;
+// b = b + 15;
+// c = c + 'I am a';
+
+
+// ~~~8~~~
+
+// var ourName = 'Ilxom';
+// var lastName = 'Ibroximovich';
+// console.log(ourName);
+// console.log(lastName);
+
+// ~~~9~~~
+
+// var AAA = '@NugmanovI';
+// var BBB = 'new york';
+// BBB = 'Washington';
+// console.log(AAA, BBB );
+
+
+// ~~~10~~~
+
+// var sum = 12 + 10;
+
+// console.log(sum);
+
+// ~~~11~~~
+
+// var a = 1;
+// a += 5;
+// console.log(a);
+
+// ~~~12~~~
+
+// var i = 1;
+// i++;
+// console.log(i);
