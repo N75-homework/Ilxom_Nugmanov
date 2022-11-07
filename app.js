f2bbf36fc6cf1edd0ab7ce6d791f3ab04e0944ae
IlxomjonNugmanov <nugmanovilxom2003@gmail.com>
2022-11-07 18:03:30 +0500

    full

--- app.js
+++ app.js
@@ -1,91 +1,33 @@
-// ~~~1~~~
-// Comment
+alert('Iltimos malumotni kiritayotganda etiborli boling. (OTM larni namunadagidek kiriting)');
+var lastName = prompt('Iltimos ismingizni kiriting.');
+var otm = prompt( lastName + ' qaysi OTM ni tanlaysiz. 1.TDIU  2.TDMI  3.MU');
+var bal = prompt( lastName + ' ballingizni kiriting .');
 
-/*Comment 
-qatorlari*/
-
-// ~~~2~~~
-
-var lastName = 10;
-var ourName = 'Ilxom';
-var numString = '10';
-var a = null; 
-var b = undefined;
-var c = true;
-var d = fox = {
-
-} ;
-var e = 10n;
-var f = app = [
-
-];
-
-// ~~~3~~~
-
-// var myVar ;
-// myVar = 7;
-// console.log(myVar);
-
-
-// ~~~4~~~
-
-// var a;
-// a = 5;
-// var b;
-// b = a;
-// console.log(b);
-
-
-// ~~~5~~~
-
-// var a = 9;
-// console.log(a);
-
-// ~~~6~~~
-
-// var myFirstName = 'ilxpom';
-// var myLastName = 'lekjdbl';
-
-// ~~~7~~~
-
-// var a;
-// var b;
-// var c;
-
-// a = a + 6;
-// b = b + 15;
-// c = c + 'I am a';
-
-
-// ~~~8~~~
-
-// var ourName = 'Ilxom';
-// var lastName = 'Ibroximovich';
-// console.log(ourName);
-// console.log(lastName);
-
-// ~~~9~~~
-
-// var AAA = '@NugmanovI';
-// var BBB = 'new york';
-// BBB = 'Washington';
-// console.log(AAA, BBB );
-
-
-// ~~~10~~~
-
-// var sum = 12 + 10;
-
-// console.log(sum);
-
-// ~~~11~~~
-
-// var a = 1;
-// a += 5;
-// console.log(a);
-
-// ~~~12~~~
-
-// var i = 1;
-// i++;
-// console.log(i);
+if (otm === 'TDIU') {
+    if (bal <= 120) {
+        alert('Afsuski siz yiqilgansiz.');
+    } else if (bal > 120 && bal <= 150) {
+        alert('Siz kantrakt asosida oqishga qabul qilindingiz.');
+    } else{
+        alert('Siz grantga qabul qilindingiz.');
+    }
+} else if (otm === 'TDMI') {
+    if (bal <= 100) {
+        alert('Afsuski siz yiqilgansiz.');
+    } else if (bal > 100 && bal <= 160) {
+        alert('Siz kantrakt asosida oqishga qabul qilindingiz.');
+    } else{
+        alert('Siz grantga qabul qilindingiz.');
+    }
+} else if (otm === 'MU') {
+    if (bal <= 160) {
+        alert('Afsuski siz yiqilgansiz.');
+    } else if (bal > 168 && bal <= 180) {
+        alert('Siz kantrakt asosida oqishga qabul qilindingiz.');
+    } else{
+        alert('Siz grantga qabul qilindingiz.');
+    }
+}
+else{
+    alert('Siz OTM yoki boshqa malumotlarni notogri kirittingiz.');
+}
