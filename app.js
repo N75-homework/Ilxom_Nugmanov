c62f4631f8ac7271be5a614ee9f360f2734f8004
IlxomjonNugmanov <nugmanovilxom2003@gmail.com>
2022-11-11 16:00:12 +0500

    full full

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ var $reset = document.querySelector('.reset');
 var $left = document.querySelector('.left');
 var $right = document.querySelector('.right');
 var $son = document.querySelector('.son');
-var i = 1;
+var i = 0 ;
 
 
 $left.addEventListener('click' , function (){
- $son.textContent =  i--;
+ i = i-1;
+ $son.textContent =  i
 })
 $reset.addEventListener('click' , function (){
-    $son.textContent = 0;
+    i = i = 0;
+    $son.textContent = i
 })
 $right.addEventListener('click' , function (){
-    $son.textContent =  i++;
+    i = i+1;
+    $son.textContent = i
 })
